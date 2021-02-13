d37675e44a9b3210947cff59a1d6c35a1ea790e7
Louis Hollingworth <louis@hollingworth.ch>
2021-02-13 11:54:00 +0000

    Refactored UI code to make it look cleaner

--- src/main/kotlin/xyz/ludoviko/ktrl/ui/UI.kt
+++ src/main/kotlin/xyz/ludoviko/ktrl/ui/UI.kt
@@ -1,8 +1,10 @@
 package xyz.ludoviko.ktrl.ui
 
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription
+import io.github.cottonmc.cotton.gui.widget.WButton
 import io.github.cottonmc.cotton.gui.widget.WGridPanel
 import io.github.cottonmc.cotton.gui.widget.WLabel
+import net.minecraft.text.Text
 import net.minecraft.text.TranslatableText
 
 class UI : LightweightGuiDescription() {
@@ -11,8 +13,16 @@ class UI : LightweightGuiDescription() {
     init {
         setRootPanel(root)
         root.setSize(150, 150)
+        addWeather(root)
+    }
 
+    fun addWeather(root: WGridPanel) {
         val weatherLabel = WLabel(TranslatableText("text.ktrl.weather.label"))
+        val clearButton = WButton(Text.of("Clear"))
+        val rainButton = WButton(Text.of("Rain"))
+        val thunderButton = WButton(Text.of("Thunder"))
+
         root.add(weatherLabel, 0, 1)
+        root.add(clearButton, 0, 2, 3, 1)
     }
 }
