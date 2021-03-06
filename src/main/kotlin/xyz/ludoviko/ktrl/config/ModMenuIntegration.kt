b6c707379edc24cd7826d30313151d7c6cbbcad9
Louis Hollingworth <louis@hollingworth.ch>
2021-03-06 11:52:42 +0000

    Added mod menu integration.

---
+++ src/main/kotlin/xyz/ludoviko/ktrl/config/ModMenuIntegration.kt
@@ -0,0 +1,20 @@
+package xyz.ludoviko.ktrl.config
+
+import io.github.prospector.modmenu.api.ConfigScreenFactory
+import io.github.prospector.modmenu.api.ModMenuApi
+import me.shedaniel.autoconfig.AutoConfig
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+import net.minecraft.client.gui.screen.Screen
+
+
+@Environment(EnvType.CLIENT)
+class ModMenuIntegration : ModMenuApi {
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*>? {
+        return ConfigScreenFactory { parent: Screen? ->
+            AutoConfig.getConfigScreen(
+                ModConfig::class.java, parent
+            ).get()
+        }
+    }
+}
