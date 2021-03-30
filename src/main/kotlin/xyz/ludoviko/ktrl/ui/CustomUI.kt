6cac4c1d2e6524f680ce759ab4b930ebb78fa79d
Louis Hollingworth <louis@hollingworth.ch>
2021-03-30 17:29:17 +0100

    Custom UI complete! It should work! Ready for release. Bumped version number

--- src/main/kotlin/xyz/ludoviko/ktrl/ui/CustomUI.kt
+++ src/main/kotlin/xyz/ludoviko/ktrl/ui/CustomUI.kt
@@ -3,7 +3,6 @@ package xyz.ludoviko.ktrl.ui
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription
 import io.github.cottonmc.cotton.gui.widget.WButton
 import io.github.cottonmc.cotton.gui.widget.WGridPanel
-import io.github.cottonmc.cotton.gui.widget.WText
 import net.minecraft.client.MinecraftClient
 import net.minecraft.text.Text
 import xyz.ludoviko.ktrl.config.ButtonConfig
@@ -14,28 +13,59 @@ class CustomUI(val client: MinecraftClient) : LightweightGuiDescription() {
 
     init {
         setRootPanel(root)
-        root.setSize(150, 150)
+        root.setSize(150, 100)
         add()
     }
 
     fun add() {
-        if (config.amount != 0) {
-            for (i in 0 until config.amount) {
-                if (config.buttons[i.toString()]?.isNotEmpty() == true) {
-                    val button = config.buttons[i.toString()]
-                    addButton(button?.get("title")!!, button.get("cmd")!!)
-                }
-            }
-        } else {
-            root.add(WText(Text.of("No custom buttons!")), 0, 2)
-        }
+        if (config["button0.active"] as Boolean) addButton(
+            config["button0.title"] as String,
+            config["button0.command"] as String,
+            0
+        )
+        if (config["button1.active"] as Boolean) addButton(
+            config["button1.title"] as String,
+            config["button1.command"] as String,
+            1
+        )
+        if (config["button2.active"] as Boolean) addButton(
+            config["button2.title"] as String,
+            config["button2.command"] as String,
+            2
+        )
+        if (config["button3.active"] as Boolean) addButton(
+            config["button3.title"] as String,
+            config["button3.command"] as String,
+            3
+        )
+        if (config["button4.active"] as Boolean) addButton(
+            config["button4.title"] as String,
+            config["button4.command"] as String,
+            4
+        )
+        if (config["button5.active"] as Boolean) addButton(
+            config["button5.title"] as String,
+            config["button5.command"] as String,
+            5
+        )
     }
 
-    fun addButton(title: String, command: String) {
+    fun addButton(title: String, command: String, pos: Int) {
         val button = WButton(Text.of(title))
         button.setOnClick {
             client.player?.sendChatMessage(command)
         }
-        root.add(button, 0, 1)
+        if (pos == 0)
+            root.add(button, 0, 1, 5, 1)
+        if (pos == 1)
+            root.add(button, 6, 1, 5, 1)
+        if (pos == 2)
+            root.add(button, 0, 3, 5, 1)
+        if (pos == 3)
+            root.add(button, 6, 3, 5, 1)
+        if (pos == 4)
+            root.add(button, 0, 5, 5, 1)
+        if (pos == 5)
+            root.add(button, 6, 5, 5, 1)
     }
 }
